ed21ef114e70788010163fd7fbab72b9522793b7
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-01 16:59:09 +0700

    update: logic and debugging

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import random
 
-print(f"Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number.")
+print(f"\n\nWelcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number.")
+print("\n-----==========-----")
+angka_rand = random.randint(1, 100)
+
+x = int(input(f"Please select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)\nYour choose -> "))
+
+chance = 0
+if x == 1:
+    chance = 10
+elif x == 2:
+    chance = 5
+elif x == 3:
+    chance = 3
+else:
+    print("Your input is invalid")
+
+print(f"Result submit is {chance} chance")
